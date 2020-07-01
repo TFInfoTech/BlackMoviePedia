929715b470bfbef39d5621036b9d0916aff7581c
Jiyue Jia <276269189@qq.com>
2020-07-01 11:40:01 +0800

    add film detail page

--- src/router/index.js
+++ src/router/index.js
@@ -5,74 +5,83 @@ import store from "../store/index";
 Vue.use(Router);
 
 const router = new Router({
-  routes: [
-    {
-      path: "/",
-      redirect: "/index"
-    },
-    {
-      path: "/index",
-      component: () => import("@/views/index/index"),
-      meta: {
-        title: "每日生鲜",
-        // 缓存首页
-        keepAlive: true
-      }
-    },
-    {
-      path: "/cart",
-      meta: {
-        title: "购物车"
-      },
-      component: () => import("@/views/cart/index")
-    },
-    {
-      path: "/my",
-      component: () => import("@/views/my/index"),
-      redirect: "/my/center",
-      children: [
+    routes: [
         {
-          path: "center",
-          meta: {
-            title: "个人中心"
-          },
-          component: () => import("@/views/my/children/center")
+            path: "/",
+            redirect: "/index"
         },
         {
-          path: "set",
-          meta: {
-            title: "设置"
-          },
-          component: () => import("@/views/my/children/set")
+            path: "/index",
+            component: () => import("@/views/detail/index"),
+            meta: {
+                title: "黑白电影详情",
+                // 缓存首页
+                keepAlive: true
+            }
+        },
+        {
+            path: "/index",
+            component: () => import("@/views/index/index"),
+            meta: {
+                title: "黑白电影百科",
+                // 缓存首页
+           
+            }
+        },
+        {
+            path: "/cart",
+            meta: {
+                title: "购物车"
+            },
+            component: () => import("@/views/cart/index")
+        },
+        {
+            path: "/my",
+            component: () => import("@/views/my/index"),
+            redirect: "/my/center",
+            children: [
+                {
+                    path: "center",
+                    meta: {
+                        title: "个人中心"
+                    },
+                    component: () => import("@/views/my/children/center")
+                },
+                {
+                    path: "set",
+                    meta: {
+                        title: "设置"
+                    },
+                    component: () => import("@/views/my/children/set")
+                }
+            ]
+        },
+        {
+            path: "/login",
+            component: () => import("@/views/login/index"),
+            meta: {
+                title: "登陆"
+            }
+        },
+        {
+            path: "*",
+            component: () => import("@/components/NotFound")
         }
-      ]
-    },
-    {
-      path: "/login",
-      component: () => import("@/views/login/index"),
-      meta: {
-        title: "登陆"
-      }
-    },
-    {
-      path: "*",
-      component: () => import("@/components/NotFound")
-    }
-  ]
+    ]
 });
 
 router.beforeEach((to, from, next) => {
-  let { title, needLogin } = to.meta;
-  let { isLogin } = store.state;
-  document.title = title;
+    let { title, needLogin } = to.meta;
+    let { isLogin } = store.state;
+    document.title = title;
 
-  if (needLogin && !isLogin) {
-    next({
-      path: "/login"
-    });
-  } else {
-    next();
-  }
+    if (needLogin && !isLogin) {
+        next({
+            path: "/login"
+        });
+    } else {
+        next();
+    }
 });
 
 export default router;
