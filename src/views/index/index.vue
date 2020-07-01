e1d4eaf7fe9933b77adc2c78785675cfd5e4e88c
Jiyue Jia <276269189@qq.com>
2020-07-01 11:30:29 +0800

    jiyue

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div style="height:100%" class="flexClass">
         <!--<van-tabs v-model="active" @change="onchange">
             <van-tab title="樱桃"></van-tab>
             <van-tab title="牧马人"></van-tab>
@@ -13,16 +13,13 @@
                       :value="item.saleDate" />
         </van-list>-->
         <swiper ref="mySwiper" :options="swiperOptions">
-            <swiper-slide v-for="(item,index) in FilePhotoList" :key="index">
+            <swiper-slide v-for="(item,index) in FilePhotoList" :key="index" class="flexClass">
                 <img :src="item.url" class="slide-image" />
             </swiper-slide>
-            <!--<swiper-slide><img src="../../assets/img/nature-2.jpg" class="slide-image" /></swiper-slide>
-            <swiper-slide><img src="../../assets/img/nature-3.jpg" class="slide-image" /></swiper-slide>
-            <swiper-slide><img src="../../assets/img/nature-4.jpg" class="slide-image" /></swiper-slide>
-            <swiper-slide><img src="../../assets/img/nature-5.jpg" class="slide-image" /></swiper-slide>-->
-            <div class="swiper-pagination" slot="pagination"></div>
+            <div class="swiper-button-prev" slot="button-prev"></div>
+            <div class="swiper-button-next" slot="button-next"></div>
+            <!--<div class="swiper-pagination" slot="pagination"></div>-->
         </swiper>
-        <Footer page="index"></Footer>
     </div>
 </template>
 
@@ -32,7 +29,7 @@
     import * as mservice from '@/api/movie'
     // import { Tabbar, TabbarItem } from "vant";
     // import { Tab, Tabs } from "vant";
-
+    let vm = null;
     export default {
         components: {
             Slideritem: slider_item,
@@ -56,14 +53,28 @@
                     pagination: {
                         el: '.swiper-pagination'
                     },
-                    // Some Swiper option/callback...
+                    navigation: {
+                        nextEl: '.swiper-button-next',
+                        prevEl: '.swiper-button-prev'
+                    },
+                    autoplay: {
+                        delay: 5000,
+                        stopOnLastSlide: false,
+                        disableOnInteraction: true,
+                    },
+                    on: {
+                        click: function () {
+                            const realIndex = this.realIndex;
+                            vm.handleClickSlide(realIndex);
+                        }
+                    },
                 }
             };
         },
         created() {
             let index = this.active + 1;
             this.list = this[`list${index}`]; // this.list1
-            // this.getGoods();
+            vm = this;
         },
         components: {
             Footer
@@ -82,17 +93,9 @@
             onchange(index) {
                 this.list = this[`list${index + 1}`];
             },
-
-            getGoods() {
-                let url = "/goods"; // /api/goods
-                this.$axios
-                    .get(url)
-                    .then(res => {
-                        console.log("res", res);
-                    })
-                    .catch(err => {
-                        console.log("err", err);
-                    });
+            handleClickSlide(index) {
+                console.log('当前点击索引：', index);
+                console.log('当前点击电影uri', this.FilePhotoList[index])
             },
             getMovies() {
                 var query = {}
@@ -101,15 +104,17 @@
                     console.log("response", response)
                     let filmlist = response;
                     for (let i = 0; i < filmlist.length; i++) {
-                        let queryphoto = {}
+                        let queryphoto = {};
+                        let urlitem = {};
                         queryphoto.freetext = filmlist[i].name;
+                        urlitem.filmuri = filmlist[i].uri;
                         mservice.fetchPhotoByName(queryphoto).then(response => {
                             console.log("fetchPhotoByName", response);
                             let photos = response;
                             for (let i = 0; i < photos.length; i++) {
-                                if (photos[i].imgPath != null) {
-                                    let urlitem = {};
-                                    urlitem.url=photos[i].imgPath
+                                if (photos[i].imgPath != null) {                                 
+                                    urlitem.url = photos[i].imgPath;                                   
+                                    //this.FilePhotoList.length = 0;
                                     this.FilePhotoList.push(urlitem);
                                 }
                             }
@@ -117,7 +122,7 @@
                     }
 
                 })
-            }
+            },
         }
     };
 </script>
@@ -125,8 +130,11 @@
 <style>
 
     html, body {
-        position: relative;
+        /*position: relative;*/
+        width: 100%;
         height: 100%;
+        margin: 0;
+        padding: 0;
     }
 
     body {
@@ -137,24 +145,39 @@
         margin: 0;
         padding: 0;
     }
+    body div{
+        height:100%
+    }
 
     .swiper-container {
         width: 300px;
-        height: 650px;
-        position: absolute;
-        left: 50%;
-        top: 20%;
-        margin-left: -150px;
-        margin-top: -150px;
+        height: 500px;
     }
 
     .swiper-slide {
         background-position: center;
         background-size: cover;
+        background-image: url(../../assets/img/MovieBackground.jpg);
+        border-radius:20px;
     }
 
     .slide-image {
-        width: 300px;
-        height: 650px
+        width: 90%;
+        height: 100%;
+        object-fit: contain;
+    }
+    .flexClass {
+        display: flex;
+        align-items: center; /*定义body的元素垂直居中*/
+        justify-content: center; /*定义body的里的元素水平居中*/
+    }
+    .swiper-button-next {
+        right:-10px;
+    }
+    .swiper-button-prev {
+        left: -10px;
+    }
+    .swiper-button-prev, .swiper-button-next{
+        color:grey;
     }
 </style>
